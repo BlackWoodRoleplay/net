6adb6bae7e94652515ab9514df6c1feb819a04f0
CrazyDoug <douglasalvescosta@hotmail.com>
2021-06-14 12:25:58 -0300

    story att

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ jQuery.fn.toggleText = function(a,b) {
     }
     
     $(document).ready(function(){
-        $('.box').before('<span><img src="titulo/jornada.png"></span>');
+        $('.box').before('<span><img src="titulo/breve.png"></span>');
         $('.box').css('display', 'none')
         $('span', '#box_geral').click(function() {
             $(this).next().slideToggle('slow')
@@ -18,7 +18,7 @@ jQuery.fn.toggleText = function(a,b) {
 
 
     $(document).ready(function(){
-        $('.box2').before('<span><img src="titulo/cidade.png"></span>');
+        $('.box2').before('<span><img src="titulo/breve.png"></span>');
         $('.box2').css('display', 'none')
         $('span', '#box_geral2').click(function() {
             $(this).next().slideToggle('slow')
@@ -32,7 +32,7 @@ jQuery.fn.toggleText = function(a,b) {
 
 
     $(document).ready(function(){
-        $('.box3').before('<span><img src="titulo/portal.png"></span>');
+        $('.box3').before('<span><img src="titulo/breve.png"></span>');
         $('.box3').css('display', 'none')
         $('span', '#box_geral3').click(function() {
             $(this).next().slideToggle('slow')
@@ -45,7 +45,7 @@ jQuery.fn.toggleText = function(a,b) {
     })
 	
 	$(document).ready(function(){
-        $('.box4').before('<span><img src="titulo/furia.png"></span>');
+        $('.box4').before('<span><img src="titulo/breve.png"></span>');
         $('.box4').css('display', 'none')
         $('span', '#box_geral4').click(function() {
             $(this).next().slideToggle('slow')
