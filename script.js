2ed5c6ff93fad6d46edba00d4f04cb5b692ca10d
Doug <33207031+CrazyDoug@users.noreply.github.com>
2021-10-20 18:44:59 -0300

    Varios updates e alteraçãos

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ jQuery.fn.toggleText = function(a,b) {
     }
     
     $(document).ready(function(){
-        $('.box').before('<span><img src="titulo/story1.png"></span>');
+        $('.box').before('<span><img src="titulo/story000.png"></span>');
         $('.box').css('display', 'none')
         $('span', '#box_geral').click(function() {
             $(this).next().slideToggle('slow')
@@ -18,7 +18,7 @@ jQuery.fn.toggleText = function(a,b) {
 
 
     $(document).ready(function(){
-        $('.box2').before('<span><img src="titulo/story2.png"></span>');
+        $('.box2').before('<span><img src="titulo/story00.png"></span>');
         $('.box2').css('display', 'none')
         $('span', '#box_geral2').click(function() {
             $(this).next().slideToggle('slow')
@@ -32,7 +32,7 @@ jQuery.fn.toggleText = function(a,b) {
 
 
     $(document).ready(function(){
-        $('.box3').before('<span><img src="titulo/story3.png"></span>');
+        $('.box3').before('<span><img src="titulo/story1.png"></span>');
         $('.box3').css('display', 'none')
         $('span', '#box_geral3').click(function() {
             $(this).next().slideToggle('slow')
@@ -45,7 +45,7 @@ jQuery.fn.toggleText = function(a,b) {
     })
 	
 	$(document).ready(function(){
-        $('.box4').before('<span><img src="titulo/story4.png"></span>');
+        $('.box4').before('<span><img src="titulo/story2.png"></span>');
         $('.box4').css('display', 'none')
         $('span', '#box_geral4').click(function() {
             $(this).next().slideToggle('slow')
@@ -58,7 +58,7 @@ jQuery.fn.toggleText = function(a,b) {
     })
 	
 	$(document).ready(function(){
-        $('.box5').before('<span><img src="titulo/breve.png"></span>');
+        $('.box5').before('<span><img src="titulo/story3.png"></span>');
         $('.box5').css('display', 'none')
         $('span', '#box_geral5').click(function() {
             $(this).next().slideToggle('slow')
@@ -70,7 +70,44 @@ jQuery.fn.toggleText = function(a,b) {
     });
     })
 
+    $(document).ready(function(){
+        $('.box6').before('<span><img src="titulo/story4.png"></span>');
+        $('.box6').css('display', 'none')
+        $('span', '#box_geral6').click(function() {
+            $(this).next().slideToggle('slow')
+            .siblings('.tgl:visible').slideToggle('fast');
+    // aqui começa o funcionamento do plugin
+            $(this).toggleText('Mostrar','Esconder')
+            .siblings('span').next('.tgl:visible').prev()
+            .toggleText('Mostrar','Esconder')
+    });
+    })
 
+    $(document).ready(function(){
+        $('.box7').before('<span><img src="titulo/breve.png"></span>');
+        $('.box7').css('display', 'none')
+        $('span', '#box_geral7').click(function() {
+            $(this).next().slideToggle('slow')
+            .siblings('.tgl:visible').slideToggle('fast');
+    // aqui começa o funcionamento do plugin
+            $(this).toggleText('Mostrar','Esconder')
+            .siblings('span').next('.tgl:visible').prev()
+            .toggleText('Mostrar','Esconder')
+    });
+    })
+
+    $(document).ready(function(){
+        $('.box8').before('<span><img src="titulo/breve.png"></span>');
+        $('.box8').css('display', 'none')
+        $('span', '#box_geral8').click(function() {
+            $(this).next().slideToggle('slow')
+            .siblings('.tgl:visible').slideToggle('fast');
+    // aqui começa o funcionamento do plugin
+            $(this).toggleText('Mostrar','Esconder')
+            .siblings('span').next('.tgl:visible').prev()
+            .toggleText('Mostrar','Esconder')
+    });
+    })    
 
     // Toggle menu on click
 // const nav = document.querySelector('#header nav')
