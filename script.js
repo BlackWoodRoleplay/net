f91b1491a3d523a2847346c91d84521cd2b880b6
Doug <33207031+CrazyDoug@users.noreply.github.com>
2022-09-23 14:18:07 -0300

    2.0.2
    varias alterações para season 3

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ jQuery.fn.toggleText = function(a,b) {
     }
     
     $(document).ready(function(){
-        $('.box').before('<span><img src="titulo/story000.png"></span>');
+        $('.box').before('<span><img src="titulo/breve.png"></span>');
         $('.box').css('display', 'none')
         $('span', '#box_geral').click(function() {
             $(this).next().slideToggle('slow')
@@ -18,7 +18,7 @@ jQuery.fn.toggleText = function(a,b) {
 
 
     $(document).ready(function(){
-        $('.box2').before('<span><img src="titulo/story00.png"></span>');
+        $('.box2').before('<span><img src="titulo/breve.png"></span>');
         $('.box2').css('display', 'none')
         $('span', '#box_geral2').click(function() {
             $(this).next().slideToggle('slow')
@@ -32,7 +32,7 @@ jQuery.fn.toggleText = function(a,b) {
 
 
     $(document).ready(function(){
-        $('.box3').before('<span><img src="titulo/story1.png"></span>');
+        $('.box3').before('<span><img src="titulo/breve.png"></span>');
         $('.box3').css('display', 'none')
         $('span', '#box_geral3').click(function() {
             $(this).next().slideToggle('slow')
