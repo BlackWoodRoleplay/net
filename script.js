427c5652c0d620083d7e30ad8eeaa5d3abb046c1
Pedro Cardoso <pedrohfsc96@gmail.com>
2022-10-25 00:45:47 -0300

    2.7

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-
+// Scroll Reveal
 window.addEventListener("scroll", function(){
         var header = document.querySelector("header");
         header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+
+
+// Botão voltar ao topo
+
+const scrollAnimation = document.querySelectorAll('[data-anima="scroll"]')
+
+
+function animationScroll(){
+        
+}
+
+
+window.addEventListener('scroll', animationScroll)
+
+
+
+// Classes
+
+function show(id) {
+        if (document.getElementById(id).style.display !== "none") {
+          document.getElementById(id).style.display = "none";
+          return;
+        }
+        Array.from(document.getElementsByClassName("hidden")).forEach(
+          div => (div.style.display = "none")
+        );
+        document.getElementById(id).style.display = "block";
+}
+
 
 // jQuery.fn.toggleText = function(a,b) {
 //     return this.html(this.html().replace(new RegExp("("+a+"|"+b+")"),function(x){return(x==a)?b:a;}));
