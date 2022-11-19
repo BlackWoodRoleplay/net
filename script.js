ef18044993813ff27aa0fc10de0ab10adde8bf90
Doug <33207031+CrazyDoug@users.noreply.github.com>
2022-11-19 03:28:44 -0300

    2.16

--- script.js
+++ script.js
@@ -4,31 +4,6 @@ window.addEventListener("scroll", function(){
         header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-
-
-// Botão voltar ao topo
-
-
-
-function animationScroll(){
-  const halfWindow = window.innerHeight * 3.5;
-  console.log(halfWindow);
-  const scrollAnimation = document.querySelector('[data-anima="scroll"]');
-  const topItem = scrollAnimation.getBoundingClientRect().top;
-
-  const itemVisible = topItem - halfWindow < 0;
-  if(itemVisible){
-    scrollAnimation.classList.add("show-button");
-  } else {
-    scrollAnimation.classList.remove("show-button");
-  };
-        
-};
-
-window.addEventListener('scroll', animationScroll);
-
-
-
 // Página Classes
 
 function show(id) {
